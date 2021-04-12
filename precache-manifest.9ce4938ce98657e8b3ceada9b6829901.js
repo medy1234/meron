2734b66b33daba8660c861174943c1cf9a26ffeb
Medy <meronhailekiros@gmail.com>
2021-04-12 17:24:22 -0400

    Updates

---
+++ precache-manifest.9ce4938ce98657e8b3ceada9b6829901.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f6fa6f46579c31200b965a8fa1c19d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0b9a22572d6757388ab",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "63b2b6b8b768a260eecd",
+    "url": "/static/js/2.c34732e1.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.c34732e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0b9a22572d6757388ab",
+    "url": "/static/js/main.f4c7100e.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "ae12851289fd4d30460ec8e3f8ad9611",
+    "url": "/static/media/hashirshoaeb.ae128512.png"
+  }
+]);
